822ee2fe9398f5b7ac973dd40fe33798346eed6e
jonaslindstrom <jonas.lindstrom@hyperisland.se>
2015-10-08 15:48:09 +0200

    Update
    Update

--- javascript.js
+++ javascript.js
@@ -842,146 +842,48 @@ chart.on('created', function() {
 /* ALLAS GRAFER */
 
 
-var chart = new Chartist.Line('.ct-chart-six', {
+/* var chart = new Chartist.Line('.ct-chart-six', {
   labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12','13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32'],
   series: [
-    /* MOBILE */
+    /* MOBILE 
     [30, 26, 24, 31, 27, 19, 25, 28, 28, 20, 23, 19, 27, 21, 26, 27, 23, 23, 33],
     [26, 22, 43, 28, 21, 24, 23, 23, 23, 24, 25, 25, 22, 30, 21, 23, 20],
-    /* INTERACTIVE ART DIRECTORZ */
+    INTERACTIVE ART DIRECTORZ 
     [25, 26, 40, 25, 31, 25, 26, 21, 23, 31, 28, 27, 30, 28, 25, 29, 28, 26, 34, 28, 26, 34, 28, 24, 25, 28, 27, 27, 25, 29],
     [35, 26, 28, 23, 25, 26, 30, 27, 30, 33, 28, 24, 25, 27, 35, 25, 34, 23, 25, 27, 48, 35, 24, 26, 26, 29, 27, 24, 32, 29, 28, 30],
-    /*DIGITAL BUSINESS */
+    /*DIGITAL BUSINESS 
     [34, 29, 23, 34, 22, 24, 25, 29, 23, 24, 23, 26, 32, 25, 27, 27, 28, 20, 22, 27, 24],
     [26, 26, 19, 28, 27, 22, 28, 25, 23, 25, 21, 25, 25, 23, 25, 24, 24, 28, 22],
-    /*MOTION */
+    /*MOTION 
     [20, 19, 28, 24, 22, 26, 24, 24, 22, 21, 30, 20, 22, 26, 23, 21, 21, 26, 24, 24],
     [28, 21, 23, 35, 25, 25, 30, 27, 27, 37, 22, 23, 27, 31, 33, 22, 19, 27, 22, 23, 22, 22],
-    /* DMC */
+    /* DMC 
     [24, 32, 21, 24, 30, 23, 26, 23, 22, 21, 22, 20, 22, 25, 23, 23, 23, 27, 34, 24, 34, 20, 25, 29],
     [23, 31, 21, 30, 33, 33, 25, 24, 27, 25, 24, 20, 25, 22, 22, 29, 29, 24, 37, 22, 21, 22, 22, 22, 23, 32],
-    /* DDS */
+    /* DDS 
     [22, 26, 28, 25, 27, 25, 27, 27, 31, 29, 28, 32, 33, 22, 24, 24, 23, 25],
     [41, 23, 33, 25, 26, 29, 36, 24, 29, 33, 20, 25, 26, 24, 31, 31, 30, 25, 26, 29],
   ]
+}, { */
+
+  new Chartist.Bar('.ct-chart-six', {
+  labels: ['19', '20', '21', '22', '23', '24', '25'],
+  series: [
+    [800000, 1200000, 1400000, 1300000],
+    [200000, 400000, 500000, 300000],
+    [100000, 200000, 400000, 600000]
+  ]
 }, {
-  low: 15
-
-});
-
-// Let's put a sequence number aside so we can use it in the event callbacks
-var seq = 0,
-  delays = 50,
-  durations = 1500;
-
-// Once the chart is fully created we reset the sequence
-chart.on('created', function() {
-  seq = 0;
-});
-
-// On each drawn element by Chartist we use the Chartist.Svg API to trigger SMIL animations
-chart.on('draw', function(data) {
-  seq++;
-
-  if(data.type === 'line') {
-    // If the drawn element is a line we do a simple opacity fade in. This could also be achieved using CSS3 animations.
-    data.element.animate({
-      opacity: {
-        // The delay when we like to start the animation
-        begin: seq * delays + 1000,
-        // Duration of the animation
-        dur: durations,
-        // The value where the animation should start
-        from: 0,
-        // The value where it should end
-        to: 1
-      }
+  stackBars: true,
+  axisY: {
+    labelInterpolationFnc: function(value) {
+      return (value / 1000) + 'k';
+    }
+  }
+}).on('draw', function(data) {
+  if(data.type === 'bar') {
+    data.element.attr({
+      style: 'stroke-width: 30px'
     });
-  } else if(data.type === 'label' && data.axis === 'x') {
-    data.element.animate({
-      y: {
-        begin: seq * delays,
-        dur: durations,
-        from: data.y + 100,
-        to: data.y,
-        // We can specify an easing function from Chartist.Svg.Easing
-        easing: 'easeOutQuart'
-      }
-    });
-  } else if(data.type === 'label' && data.axis === 'y') {
-    data.element.animate({
-      x: {
-        begin: seq * delays,
-        dur: durations,
-        from: data.x - 100,
-        to: data.x,
-        easing: 'easeOutQuart'
-      }
-    });
-  } else if(data.type === 'point') {
-    data.element.animate({
-      x1: {
-        begin: seq * delays,
-        dur: durations,
-        from: data.x - 10,
-        to: data.x,
-        easing: 'easeOutQuart'
-      },
-      x2: {
-        begin: seq * delays,
-        dur: durations,
-        from: data.x - 10,
-        to: data.x,
-        easing: 'easeOutQuart'
-      },
-      opacity: {
-        begin: seq * delays,
-        dur: durations,
-        from: 0,
-        to: 1,
-        easing: 'easeOutQuart'
-      }
-    });
-  } else if(data.type === 'grid') {
-    // Using data.axis we get x or y which we can use to construct our animation definition objects
-    var pos1Animation = {
-      begin: seq * delays,
-      dur: durations,
-      from: data[data.axis.units.pos + '1'] - 30,
-      to: data[data.axis.units.pos + '1'],
-      easing: 'easeOutQuart'
-    };
-
-    var pos2Animation = {
-      begin: seq * delays,
-      dur: durations,
-      from: data[data.axis.units.pos + '2'] - 100,
-      to: data[data.axis.units.pos + '2'],
-      easing: 'easeOutQuart'
-    };
-
-    var animations = {};
-    animations[data.axis.units.pos + '1'] = pos1Animation;
-    animations[data.axis.units.pos + '2'] = pos2Animation;
-    animations['opacity'] = {
-      begin: seq * delays,
-      dur: durations,
-      from: 0,
-      to: 1,
-      easing: 'easeOutQuart'
-    };
-
-    data.element.animate(animations);
   }
 });
-
-// For the sake of the example we update the chart every time it's created with a delay of 10 seconds
-chart.on('created', function() {
-  if(window.__exampleAnimateTimeout) {
-    clearTimeout(window.__exampleAnimateTimeout);
-    window.__exampleAnimateTimeout = null;
-  }
-  window.__exampleAnimateTimeout = setTimeout(chart.update.bind(chart), 50000);
-});
-
-
